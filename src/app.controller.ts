11798f71053f5b02e9ddc854ff3a0301d0a4ea9e
incappearance-tech <incappearance@gmail.com>
2026-02-15 23:12:34 +0530

    feat: Add API endpoint to trigger Prisma database push and remove it from the `start:prod` script.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,10 +3,28 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('init-database')
+  async initDatabase() {
+    return new Promise((resolve) => {
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
+      const { exec } = require('child_process');
+      exec('npx prisma db push --accept-data-loss', (error: any, stdout: any, stderr: any) => {
+        if (error) {
+          console.error(`exec error: ${error}`);
+          resolve(`Error: ${error.message} \nStderr: ${stderr}`);
+          return;
+        }
+        console.log(`stdout: ${stdout}`);
+        console.log(`stderr: ${stderr}`);
+        resolve(`Success! Database synced.\nStdout: ${stdout}`);
+      });
+    });
   }
 }
